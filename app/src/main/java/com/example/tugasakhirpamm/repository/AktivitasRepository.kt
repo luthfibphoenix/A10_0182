760fa8cc4b32ef8e782820da9f2b73373aea32bd
luthfibphoenix <lutfibahtiar2409@gmail.com>
2025-01-23 22:43:06 +0700

    memperbaiki pada file AktivitasRepository.kt

--- app/src/main/java/com/example/tugasakhirpamm/repository/AktivitasRepository.kt
+++ app/src/main/java/com/example/tugasakhirpamm/repository/AktivitasRepository.kt
@@ -1,2 +1,47 @@
 package com.example.tugasakhirpamm.repository
 
+import com.example.tugasakhirpamm.model.Aktivitas
+import com.example.tugasakhirpamm.service.AktivitasService
+
+interface AktivitasRepository {
+    suspend fun getAktivitas(): List<Aktivitas>
+    suspend fun insertAktivitas(aktivitas: Aktivitas)
+    suspend fun updateAktivitas(idAktivitas: String, aktivitas: Aktivitas) // String
+    suspend fun deleteAktivitas(idAktivitas: String) // String
+    suspend fun getAktivitasById(idAktivitas: String): Aktivitas // String
+}
+
+class NetworkAktivitasRepository(
+    private val aktivitasApiService: AktivitasService
+) : AktivitasRepository{
+
+    override suspend fun getAktivitas(): List<Aktivitas> {
+        return aktivitasApiService.getAktivitas()
+    }
+
+    override suspend fun getAktivitasById(idAktivitas: String): Aktivitas {
+        return aktivitasApiService.getAktivitasById(idAktivitas)
+    }
+
+    override suspend fun insertAktivitas(aktivitas: Aktivitas) {
+        aktivitasApiService.insertAktivitas(aktivitas)
+    }
+
+    override suspend fun updateAktivitas(idAktivitas: String, aktivitas: Aktivitas) {
+        aktivitasApiService.updateAktivitas(idAktivitas, aktivitas)
+    }
+
+    override suspend fun deleteAktivitas(idAktivitas: String) {
+        try{
+            val response = aktivitasApiService.deleteAktivitas(idAktivitas)
+            if (!response.isSuccessful){
+                throw Exception("Failed to delete aktivitas. HTTP Status code: " + "${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception){
+            throw e
+        }
+    }
+}
--- app/src/main/java/com/example/tugasakhirpamm/repository/CatatanRepository.kt
+++ app/src/main/java/com/example/tugasakhirpamm/repository/CatatanRepository.kt
@@ -1,2 +1,6 @@
 package com.example.tugasakhirpamm.repository
 
+import com.example.tugasakhirpamm.model.Aktivitas
+import com.example.tugasakhirpamm.model.Pekerja
+import com.example.tugasakhirpamm.service.AktivitasService
+
