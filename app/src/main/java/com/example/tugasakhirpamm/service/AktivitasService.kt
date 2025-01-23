63568042f3794afaf2d7794cfa42f7e94accb28d
luthfibphoenix <lutfibahtiar2409@gmail.com>
2025-01-23 21:46:37 +0700

    membuat interface AktivitasService

---
+++ app/src/main/java/com/example/tugasakhirpamm/service/AktivitasService.kt
@@ -0,0 +1,10 @@
+package com.example.tugasakhirpamm.service
+
+import retrofit2.http.Headers
+
+interface AktivitasService{
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+}
