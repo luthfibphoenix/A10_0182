6b7b465e72bccc539bf47c909d922dc8d5d58ff2
luthfibphoenix <lutfibahtiar2409@gmail.com>
2025-01-24 10:50:18 +0700

    membuat UpdateTanamanView.kt dan UpdateTanamanViewModel.kt

---
+++ app/src/main/java/com/example/tugasakhirpamm/ui/view/Tanaman/UpdateTanamanView.kt
@@ -0,0 +1,68 @@
+package com.example.tugasakhirpamm.ui.view.Tanaman
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.tugasakhirpamm.ui.PenyediaViewModel
+import com.example.tugasakhirpamm.ui.costumwidget.CostumeTopAppBar
+import com.example.tugasakhirpamm.ui.navigasi.DestinasiNavigasi
+import com.example.tugasakhirpamm.ui.viewmodel.Tanaman.UpdateTanamanViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiTanamanUpdate : DestinasiNavigasi {
+    override val route = "update"
+    override val titleRes = "Update Tanaman"
+    const val TANAMAN = "id Tanaman"
+    val routeWithArg = "$route/{$TANAMAN}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateScreen(
+    modifier: Modifier = Modifier,
+    viewModel: UpdateTanamanViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    navigateBack: () -> Unit,
+    onNavigate: () -> Unit
+){
+    val insertUiState = viewModel.uiState
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold (
+        modifier = Modifier,
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiTanamanUpdate.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        }
+    ){
+        padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(padding)
+                .padding(15.dp)
+        ){
+            EntryBody(
+                insertUiState = insertUiState,
+                onMahasiswaValueChange = { updateEvent ->
+                    viewModel.updateState(updateEvent)
+                },
+                onSaveClick = {
+                    coroutineScope.launch {
+                        viewModel.updateTanaman()
+                        onNavigate()
+                    }
+                }
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tugasakhirpamm/ui/viewmodel/Tanaman/UpdateTanamanViewModel.kt
@@ -0,0 +1,51 @@
+package com.example.tugasakhirpamm.ui.viewmodel.Tanaman
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.tugasakhirpamm.model.Tanaman
+import com.example.tugasakhirpamm.repository.TanamanRepository
+import com.example.tugasakhirpamm.ui.view.Tanaman.DestinasiTanamanUpdate
+import kotlinx.coroutines.launch
+
+class UpdateTanamanViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val tanamanRepository: TanamanRepository
+): ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    private val _idTanaman: String = checkNotNull(savedStateHandle[DestinasiTanamanUpdate.TANAMAN])
+
+    init {
+        viewModelScope.launch {
+            val tanaman = tanamanRepository.getTanamanById(_idTanaman)
+            uiState = tanaman.toUiStateTnmUpdate()
+        }
+    }
+
+    fun updateTanaman() {
+        viewModelScope.launch {
+            try {
+                val tanaman = uiState.insertUiEvent.toTanaman()
+                tanamanRepository.updateTanaman(_idTanaman, tanaman)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun updateState(insertUiEvent: InsertUiEvent) {
+        uiState = InsertUiState(
+            insertUiEvent = insertUiEvent
+        )
+    }
+}
+
+fun Tanaman.toUiStateTnmUpdate() = InsertUiState(
+    insertUiEvent = this.toInsertUiEvent()
+)
