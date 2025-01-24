b409244873eb35e0f0579889f36b642fd4e078d5
luthfibphoenix <lutfibahtiar2409@gmail.com>
2025-01-24 08:50:08 +0700

    memperbaiki pada class PertanianContainer

--- app/src/main/java/com/example/tugasakhirpamm/dependenciesinjection/PertanianContainer.kt
+++ app/src/main/java/com/example/tugasakhirpamm/dependenciesinjection/PertanianContainer.kt
@@ -1,49 +1,63 @@
 package com.example.tugasakhirpamm.dependenciesinjection
 
 import com.example.tugasakhirpamm.repository.AktivitasRepository
+import com.example.tugasakhirpamm.repository.CatatanRepository
+import com.example.tugasakhirpamm.repository.NetworkAktivitasRepository
+import com.example.tugasakhirpamm.repository.NetworkCatatanRepository
+import com.example.tugasakhirpamm.repository.NetworkPekerjaRepository
+import com.example.tugasakhirpamm.repository.NetworkTanamanRepository
+import com.example.tugasakhirpamm.repository.PekerjaRepository
+import com.example.tugasakhirpamm.repository.TanamanRepository
 import com.example.tugasakhirpamm.service.AktivitasService
+import com.example.tugasakhirpamm.service.CatatanService
+import com.example.tugasakhirpamm.service.PekerjaService
+import com.example.tugasakhirpamm.service.TanamanService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
-interface AppContainer{
-//    val tanamanRepository: TanamanRepository
-//    val pekerjasaRepository: PekerjasaRepository
-//    val catatanRepository: CatatanRepository
+interface AppContainer {
+    val tanamanRepository: TanamanRepository
+    val pekerjaRepository: PekerjaRepository
+    val catatanRepository: CatatanRepository
     val aktivitasRepository: AktivitasRepository
 }
 
-class PertanianContainer : AppContainer{
+class PertanianContainer : AppContainer {
     private val baseUrl = "http://10.0.2.2:83/api/kangpijat/"
-    private val json = Json{ignoreUnknownKeys = true}
+    private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(baseUrl)
         .build()
 
-    private val aktivitasService:AktivitasService by lazy {
+    private val aktivitasService: AktivitasService by lazy {
         retrofit.create(AktivitasService::class.java)
     }
-    private val aktivitasService:AktivitasService by lazy {
-        retrofit.create(AktivitasService::class.java)
+    override val aktivitasRepository: AktivitasRepository by lazy {
+        NetworkAktivitasRepository(aktivitasService)
     }
-    private val aktivitasService:AktivitasService by lazy {
-        retrofit.create(AktivitasService::class.java)
+
+    private val pekerjaService: PekerjaService by lazy {
+        retrofit.create(PekerjaService::class.java)
     }
-    private val aktivitasService:AktivitasService by lazy {
-        retrofit.create(AktivitasService::class.java)
+    override val pekerjaRepository: PekerjaRepository by lazy {
+        NetworkPekerjaRepository(pekerjaService)
     }
-    private val aktivitasService:AktivitasService by lazy {
-        retrofit.create(AktivitasService::class.java)
+
+    private val tanamanService: TanamanService by lazy {
+        retrofit.create(TanamanService::class.java)
     }
-    private val aktivitasService:AktivitasService by lazy {
-        retrofit.create(AktivitasService::class.java)
+    override val tanamanRepository: TanamanRepository by lazy {
+        NetworkTanamanRepository(tanamanService)
     }
-    private val aktivitasService:AktivitasService by lazy {
-        retrofit.create(AktivitasService::class.java)
+
+    private val catatanService: CatatanService by lazy {
+        retrofit.create(CatatanService::class.java)
     }
-    private val aktivitasService:AktivitasService by lazy {
-        retrofit.create(AktivitasService::class.java)
+    override val catatanRepository: CatatanRepository by lazy {
+        NetworkCatatanRepository(catatanService)
     }
+
 }
